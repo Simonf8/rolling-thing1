b1b60167244083b88aac488a7f3826eb31c92f89
Simonf8 <111576038+Simonf8@users.noreply.github.com>
2025-01-20 12:11:37 +0100

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,30 @@
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+    },
+    'formatters': {
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
+
+# Reduce debug output
+DEBUG = False  # Set to True only when needed
+TEMPLATE_DEBUG = False 
